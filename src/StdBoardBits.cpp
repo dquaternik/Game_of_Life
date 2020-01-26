d3209bf16e2adc33a5e9245c1f1309c6c4c1a85e
dquaternik <devon.quaternik@gmail.com>
2020-01-25 17:03:02 -0700

    Add all the stationary StdBoardBits

--- src/StdBoardBits.cpp
+++ src/StdBoardBits.cpp
@@ -100,7 +100,15 @@ BoardBit StdBoardBits::StillLife::boat()
     . x . 
     */
     StdBoardBits stdBrdBits;
-    BoardBit boat( 4, 4, 16 );
+    BoardBit boat( 3, 3, 9 );
+
+    boat.bitPosLiveList.push_back( CellPos( 0, 0, 0 ) );
+    boat.bitPosLiveList.push_back( CellPos( 1, 0, 1 ) );
+    boat.bitPosLiveList.push_back( CellPos( 0, 1, 3 ) );
+    boat.bitPosLiveList.push_back( CellPos( 2, 1, 5 ) );
+    boat.bitPosLiveList.push_back( CellPos( 1, 2, 7 ) );
+
+    boat.bitBoard = stdBrdBits.setupBoard( boat.bitPosLiveList, 3, 3 );
 
     return boat;
 }
@@ -113,7 +121,14 @@ BoardBit StdBoardBits::StillLife::tub()
     . x . 
     */
     StdBoardBits stdBrdBits;
-    BoardBit tub( 4, 4, 16 );
+    BoardBit tub( 3, 3, 9 );
+
+    tub.bitPosLiveList.push_back( CellPos( 1, 0, 1 ) );
+    tub.bitPosLiveList.push_back( CellPos( 0, 1, 3 ) );
+    tub.bitPosLiveList.push_back( CellPos( 2, 1, 5 ) );
+    tub.bitPosLiveList.push_back( CellPos( 1, 2, 7 ) );
+
+    tub.bitBoard = stdBrdBits.setupBoard( tub.bitPosLiveList, 3, 3 );
 
     return tub;
 }
@@ -126,7 +141,13 @@ BoardBit StdBoardBits::Oscillators::blinker()
     . x . 
     */
     StdBoardBits stdBrdBits;
-    BoardBit blinker( 4, 4, 16 );
+    BoardBit blinker( 3, 3, 9 );
+
+    blinker.bitPosLiveList.push_back( CellPos( 1, 0, 1 ) );
+    blinker.bitPosLiveList.push_back( CellPos( 1, 1, 4 ) );
+    blinker.bitPosLiveList.push_back( CellPos( 1, 2, 7 ) );
+
+    blinker.bitBoard = stdBrdBits.setupBoard( blinker.bitPosLiveList, 3, 3 );
 
     return blinker;
 }
@@ -142,6 +163,15 @@ BoardBit StdBoardBits::Oscillators::toad()
     StdBoardBits stdBrdBits;
     BoardBit toad( 4, 4, 16 );
 
+    toad.bitPosLiveList.push_back( CellPos( 2, 0, 2 ) );
+    toad.bitPosLiveList.push_back( CellPos( 0, 1, 4 ) );
+    toad.bitPosLiveList.push_back( CellPos( 3, 1, 7 ) );
+    toad.bitPosLiveList.push_back( CellPos( 0, 2, 8 ) );
+    toad.bitPosLiveList.push_back( CellPos( 3, 2, 11 ) );
+    toad.bitPosLiveList.push_back( CellPos( 1, 3, 13 ) );
+
+    toad.bitBoard = stdBrdBits.setupBoard( toad.bitPosLiveList, 4, 4 );
+
     return toad;
 }
 
@@ -156,6 +186,15 @@ BoardBit StdBoardBits::Oscillators::beacon()
     StdBoardBits stdBrdBits;
     BoardBit beacon( 4, 4, 16 );
 
+    beacon.bitPosLiveList.push_back( CellPos( 0, 0, 0 ) );
+    beacon.bitPosLiveList.push_back( CellPos( 1, 0, 1 ) );
+    beacon.bitPosLiveList.push_back( CellPos( 0, 1, 4 ) );
+    beacon.bitPosLiveList.push_back( CellPos( 3, 2, 11 ) );
+    beacon.bitPosLiveList.push_back( CellPos( 2, 3, 14 ) );
+    beacon.bitPosLiveList.push_back( CellPos( 3, 3, 15 ) );
+
+    beacon.bitBoard = stdBrdBits.setupBoard( beacon.bitPosLiveList, 4, 4 );
+
     return beacon;
 
 }
@@ -163,35 +202,110 @@ BoardBit StdBoardBits::Oscillators::beacon()
 BoardBit StdBoardBits::Oscillators::pulsar()
 {
     /* Needs at least one buffer
-    . . x x x . . . x x x . . 
-    . . . . . . . . . . . . . 
-    x . . . . x . x . . . . x 
-    x . . . . x . x . . . . x  
-    x . . . . x . x . . . . x
-    . . x x x . . . x x x . .
-    . . . . . . . . . . . . . 
-    . . x x x . . . x x x . .
-    x . . . . x . x . . . . x 
-    x . . . . x . x . . . . x  
-    x . . . . x . x . . . . x
-    . . . . . . . . . . . . . 
-    . . x x x . . . x x x . .
+     x
+     0 1 2 3 4 5 6 7 8 9 101112
+ y0  . . x x x . . . x x x . . 
+  1  . . . . . . . . . . . . . 
+  2  x . . . . x . x . . . . x 
+  3  x . . . . x . x . . . . x  
+  4  x . . . . x . x . . . . x
+  5  . . x x x . . . x x x . .
+  6  . . . . . . . . . . . . . 
+  7  . . x x x . . . x x x . .
+  8  x . . . . x . x . . . . x 
+  9  x . . . . x . x . . . . x  
+  0  x . . . . x . x . . . . x
+  1  . . . . . . . . . . . . . 
+  2  . . x x x . . . x x x . .
     */
     StdBoardBits stdBrdBits;
-    BoardBit pulsar( 4, 4, 16 );
+    BoardBit pulsar( 13, 13, 169 );
+
+    // upper left
+    pulsar.bitPosLiveList.push_back( CellPos( 2, 0, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 3, 0, 3 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 4, 0, 4 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 0, 2, 26 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 5, 2, 31 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 0, 3, 39 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 5, 3, 44 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 0, 4, 52 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 5, 4, 57 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 2, 5, 67 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 3, 5, 68 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 4, 5, 69 ) );
+
+    // upper right
+    pulsar.bitPosLiveList.push_back( CellPos( 8, 0, 8 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 9, 0, 9 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 10, 0, 10 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 7, 2, 33 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 12, 2, 38 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 7, 3, 46 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 12, 3, 51 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 7, 4, 59 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 12, 4, 64 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 8, 5, 73 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 9, 5, 74 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 10, 5, 75 ) );
+
+    // lower left
+    pulsar.bitPosLiveList.push_back( CellPos( 2, 7, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 3, 7, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 4, 7, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 0, 9, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 5, 9, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 0, 10, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 5, 10, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 0, 11, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 5, 11, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 2, 12, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 3, 12, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 4, 12, 2 ) );
+
+    // lower right
+    pulsar.bitPosLiveList.push_back( CellPos( 8, 7, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 9, 7, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 10, 7, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 7, 9, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 12, 9, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 7, 10, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 12, 10, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 7, 11, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 12, 11, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 8, 12, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 9, 12, 2 ) );
+    pulsar.bitPosLiveList.push_back( CellPos( 10, 12, 2 ) );   
+
+    pulsar.bitBoard = stdBrdBits.setupBoard( pulsar.bitPosLiveList, 13, 13 );
 
     return pulsar;
 }
 
 BoardBit StdBoardBits::Oscillators::pentadecathalon()
 {
-    /* Lots of space needed
+    /* Lots of buffer needed
     . . x . . . . x . . 
     x x . x x x x . x x 
     . . x . . . . x . . 
     */
     StdBoardBits stdBrdBits;
-    BoardBit pentadecathalon( 4, 4, 16 );
+    BoardBit pentadecathalon( 10, 3, 30 );
+
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 2, 0, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 7, 0, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 0, 1, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 1, 1, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 3, 1, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 4, 1, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 5, 1, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 6, 1, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 8, 1, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 9, 1, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 2, 2, 2 ) );
+    pentadecathalon.bitPosLiveList.push_back( CellPos( 7, 2, 2 ) );
+
+    pentadecathalon.bitBoard = stdBrdBits.setupBoard( pentadecathalon.bitPosLiveList, 10, 3 );
 
     return pentadecathalon;
     
